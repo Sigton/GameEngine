b7a1222fc4de9332d6a8fe1c00d06bf62dfba832
Jamie Buttenshaw <sigton.scratch@gmail.com>
2020-12-15 20:33:49 +0000

    Trying it with multiple geometries and modifying transforms

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -12,109 +12,109 @@ public:
 	GameLayer()
 		: Layer("Game Layer")
 	{
-		// cube with normals
-		float cubeVertices2[4 * 6 * (3 + 3)] = { // num vertices per side * num sides * (components of position + components of normal)
-			// front side
-			-1.0f,  1.0f,  1.0f,		 0.0f,  0.0f,  1.0f,
-			 1.0f,  1.0f,  1.0f,		 0.0f,  0.0f,  1.0f,
-			-1.0f, -1.0f,  1.0f,		 0.0f,  0.0f,  1.0f,
-			 1.0f, -1.0f,  1.0f,		 0.0f,  0.0f,  1.0f,
-			// back side	 		 	   
-			 1.0f,  1.0f, -1.0f,		 0.0f,  0.0f, -1.0f,
-			-1.0f,  1.0f, -1.0f,		 0.0f,  0.0f, -1.0f,
-			 1.0f, -1.0f, -1.0f,		 0.0f,  0.0f, -1.0f,
-			-1.0f, -1.0f, -1.0f,		 0.0f,  0.0f, -1.0f,
-			// top side		 		 	   
-			-1.0f,  1.0f, -1.0f,		 0.0f,  1.0f,  0.0f,
-			 1.0f,  1.0f, -1.0f,		 0.0f,  1.0f,  0.0f,
-			-1.0f,  1.0f,  1.0f,		 0.0f,  1.0f,  0.0f,
-			 1.0f,  1.0f,  1.0f,		 0.0f,  1.0f,  0.0f,
-			// bottom side 			 		 		   
-			-1.0f, -1.0f,  1.0f,		 0.0f, -1.0f,  0.0f,
-			 1.0f, -1.0f,  1.0f,		 0.0f, -1.0f,  0.0f,
-			-1.0f, -1.0f, -1.0f,		 0.0f, -1.0f,  0.0f,
-			 1.0f, -1.0f, -1.0f,		 0.0f, -1.0f,  0.0f,
-			// left side	 		 		 	 	   
-			-1.0f,  1.0f, -1.0f,		-1.0f,  0.0f,  0.0f,
-			-1.0f,  1.0f,  1.0f,		-1.0f,  0.0f,  0.0f,
-			-1.0f, -1.0f, -1.0f,		-1.0f,  0.0f,  0.0f,
-			-1.0f, -1.0f,  1.0f,		-1.0f,  0.0f,  0.0f,
-			// right side 			 	 	 	   
-			 1.0f,  1.0f,  1.0f,		 1.0f,  0.0f,  0.0f,
-			 1.0f,  1.0f, -1.0f,		 1.0f,  0.0f,  0.0f,
-			 1.0f, -1.0f,  1.0f,		 1.0f,  0.0f,  0.0f,
-			 1.0f, -1.0f, -1.0f,		 1.0f,  0.0f,  0.0f,
-		};
-
-		unsigned int cubeIndices2[6 * 6] = {
-			// front
-			0, 2, 3,
-			0, 3, 1,
-			// back
-			4, 6, 7,
-			4, 7, 5,
-			// top
-			8, 10, 11,
-			8, 11, 9,
-			// bottom
-			12, 14, 15,
-			12, 15, 13,
-			// left
-			16, 18, 19,
-			16, 19, 17,
-			// right
-			20, 22, 23,
-			20, 23, 21,
-		};
-		/*
-		// define geometry and indices
-		float cubeVertices[8 * (3 + 2)] = {
-			-1.0f,	 1.0f,	 -1.0f,		0.0f, 1.0f,
-			 1.0f,	 1.0f,	 -1.0f,		1.0f, 1.0f,
-			-1.0f,	-1.0f,	 -1.0f,		0.0f, 0.0f,
-			 1.0f,	-1.0f,	 -1.0f,		1.0f, 0.0f,
-			-1.0f,	 1.0f,	  1.0f,		1.0f, 0.0f,
-			 1.0f,	 1.0f,	  1.0f,		0.0f, 0.0f,
-			-1.0f,	-1.0f,	  1.0f,		1.0f, 1.0f,
-			 1.0f,	-1.0f,	  1.0f,		0.0f, 1.0f
-		};
-
-		unsigned int cubeIndices[6 * 6] = {
-			0, 3, 1, 0, 2, 3,
-			5, 6, 4, 5, 7, 6,
-			4, 2, 0, 4, 6, 2,
-			1, 7, 5, 1, 3, 7,
-			4, 1, 5, 4, 0, 1,
-			2, 7, 3, 2, 6, 7
-		};
-
-
-		float squareVertices[4 * (3 + 4)] = {
-			-1.0f,	 1.0f,	 0.0f,		0.0f, 1.0f,
-			 1.0f,	 1.0f,	 0.0f,		1.0f, 1.0f,
-			-1.0f,	-1.0f,	 0.0f,		0.0f, 0.0f,
-			 1.0f,	-1.0f,	 0.0f,		1.0f, 0.0f
-		};
-
-		unsigned int squareIndices[6] =
 		{
-			0, 3, 1, 0, 2, 3
-		};
-		*/
-		// gen vertex array
-		m_VertexArray = GameEngine::VertexArray::Create();
+			// cube with normals
+			float cubeVertices[4 * 6 * (3 + 3)] = { // num vertices per side * num sides * (components of position + components of normal)
+				// front side
+				-1.0f,  1.0f,  1.0f,		 0.0f,  0.0f,  1.0f,
+				 1.0f,  1.0f,  1.0f,		 0.0f,  0.0f,  1.0f,
+				-1.0f, -1.0f,  1.0f,		 0.0f,  0.0f,  1.0f,
+				 1.0f, -1.0f,  1.0f,		 0.0f,  0.0f,  1.0f,
+				 // back side	 		 	   
+				  1.0f,  1.0f, -1.0f,		 0.0f,  0.0f, -1.0f,
+				 -1.0f,  1.0f, -1.0f,		 0.0f,  0.0f, -1.0f,
+				  1.0f, -1.0f, -1.0f,		 0.0f,  0.0f, -1.0f,
+				 -1.0f, -1.0f, -1.0f,		 0.0f,  0.0f, -1.0f,
+				 // top side		 		 	   
+				 -1.0f,  1.0f, -1.0f,		 0.0f,  1.0f,  0.0f,
+				  1.0f,  1.0f, -1.0f,		 0.0f,  1.0f,  0.0f,
+				 -1.0f,  1.0f,  1.0f,		 0.0f,  1.0f,  0.0f,
+				  1.0f,  1.0f,  1.0f,		 0.0f,  1.0f,  0.0f,
+				  // bottom side 			 		 		   
+				  -1.0f, -1.0f,  1.0f,		 0.0f, -1.0f,  0.0f,
+				   1.0f, -1.0f,  1.0f,		 0.0f, -1.0f,  0.0f,
+				  -1.0f, -1.0f, -1.0f,		 0.0f, -1.0f,  0.0f,
+				   1.0f, -1.0f, -1.0f,		 0.0f, -1.0f,  0.0f,
+				   // left side	 		 		 	 	   
+				   -1.0f,  1.0f, -1.0f,		-1.0f,  0.0f,  0.0f,
+				   -1.0f,  1.0f,  1.0f,		-1.0f,  0.0f,  0.0f,
+				   -1.0f, -1.0f, -1.0f,		-1.0f,  0.0f,  0.0f,
+				   -1.0f, -1.0f,  1.0f,		-1.0f,  0.0f,  0.0f,
+				   // right side 			 	 	 	   
+					1.0f,  1.0f,  1.0f,		 1.0f,  0.0f,  0.0f,
+					1.0f,  1.0f, -1.0f,		 1.0f,  0.0f,  0.0f,
+					1.0f, -1.0f,  1.0f,		 1.0f,  0.0f,  0.0f,
+					1.0f, -1.0f, -1.0f,		 1.0f,  0.0f,  0.0f,
+			};
 
-		std::shared_ptr<GameEngine::VertexBuffer> vertexBuffer = GameEngine::VertexBuffer::Create(cubeVertices2, sizeof(cubeVertices2));
+			unsigned int cubeIndices[6 * 6] = {
+				// front
+				0, 2, 3,
+				0, 3, 1,
+				// back
+				4, 6, 7,
+				4, 7, 5,
+				// top
+				8, 10, 11,
+				8, 11, 9,
+				// bottom
+				12, 14, 15,
+				12, 15, 13,
+				// left
+				16, 18, 19,
+				16, 19, 17,
+				// right
+				20, 22, 23,
+				20, 23, 21
+			};
 
-		GameEngine::BufferLayout layout = {
-			{ GameEngine::ShaderDataType::Float3, "a_Position"},
-			{ GameEngine::ShaderDataType::Float3, "a_Normal"}
-		};
-		vertexBuffer->SetLayout(layout);
-		m_VertexArray->AddVertexBuffer(vertexBuffer);
+			// gen vertex array
+			m_VertexArray = GameEngine::VertexArray::Create();
 
-		std::shared_ptr<GameEngine::IndexBuffer> indexBuffer = GameEngine::IndexBuffer::Create(cubeIndices2, sizeof(cubeIndices2) / sizeof(unsigned int));
-		m_VertexArray->SetIndexBuffer(indexBuffer);
+			std::shared_ptr<GameEngine::VertexBuffer> vertexBuffer = GameEngine::VertexBuffer::Create(cubeVertices, sizeof(cubeVertices));
+
+			GameEngine::BufferLayout layout = {
+				{ GameEngine::ShaderDataType::Float3, "a_Position"},
+				{ GameEngine::ShaderDataType::Float3, "a_Normal"}
+			};
+			vertexBuffer->SetLayout(layout);
+			m_VertexArray->AddVertexBuffer(vertexBuffer);
+
+			std::shared_ptr<GameEngine::IndexBuffer> indexBuffer = GameEngine::IndexBuffer::Create(cubeIndices, sizeof(cubeIndices) / sizeof(unsigned int));
+			m_VertexArray->SetIndexBuffer(indexBuffer);
+		}
+
+		{
+			// quad with normals
+			float quadVertices[4 * 1 * (3 + 3)] = { // num vertices per side * num sides * (components of position + components of normal)
+				// top side
+				-1.0f,  0.0f,  1.0f,		 0.0f,  1.0f,  0.0f,
+				 1.0f,  0.0f,  1.0f,		 0.0f,  1.0f,  0.0f,
+				-1.0f,  0.0f, -1.0f,		 0.0f,  1.0f,  0.0f,
+				 1.0f,  0.0f, -1.0f,		 0.0f,  1.0f,  0.0f
+			};
+
+			unsigned int quadIndices[1 * 6] = {
+				// top
+				0, 2, 3,
+				0, 3, 1
+			};
+
+			// gen vertex array
+			m_PlaneVertexArray = GameEngine::VertexArray::Create();
+
+			std::shared_ptr<GameEngine::VertexBuffer> vertexBuffer = GameEngine::VertexBuffer::Create(quadVertices, sizeof(quadVertices));
+
+			GameEngine::BufferLayout layout = {
+				{ GameEngine::ShaderDataType::Float3, "a_Position"},
+				{ GameEngine::ShaderDataType::Float3, "a_Normal"}
+			};
+			vertexBuffer->SetLayout(layout);
+			m_PlaneVertexArray->AddVertexBuffer(vertexBuffer);
+
+			std::shared_ptr<GameEngine::IndexBuffer> indexBuffer = GameEngine::IndexBuffer::Create(quadIndices, sizeof(quadIndices) / sizeof(unsigned int));
+			m_PlaneVertexArray->SetIndexBuffer(indexBuffer);
+		}
 
 		m_Shader = GameEngine::Shader::Create("assets/shaders/BasicDiffuse.glsl");
 
@@ -124,18 +124,37 @@ public:
 
 		m_Camera = std::make_shared<GameEngine::PerspectiveCamera>();
 		m_CameraPosition = glm::vec3(0.0f, 0.0f, 5.0f);
+		m_CameraSmoothVector = glm::vec2(0);
 
 		m_Transform = std::make_shared<GameEngine::Transform>();
 		m_SquareScale = glm::vec3(1.0f);
+
+		m_LightPos = glm::vec3(0.0f, 3.0f, 0.0f);
 		
+		m_PlaneTransform = std::make_shared<GameEngine::Transform>();
+		m_PlaneTransform->SetPosition(glm::vec3(0.0f, -2.0f, 0.0f));
+		m_PlaneTransform->SetScale(glm::vec3(10));
 	}
 
 	virtual void OnAttach() override
 	{
 	}
 
-	virtual void OnUpdate() override
+	virtual void OnUpdate(GameEngine::Timestep ts) override
 	{
+
+		// camera controlled by the mouse
+		glm::vec2 md = GameEngine::Input::GetMouseDelta();
+		md *= m_CameraSensitivity * m_CameraSmoothing;
+		m_CameraSmoothVector = glm::mix(m_CameraSmoothVector, md, 1.0f / m_CameraSmoothing);
+		m_MouseLook += m_CameraSmoothVector;
+
+		m_CameraRotation = glm::vec3(-glm::radians(m_MouseLook.y), -glm::radians(m_MouseLook.x), 0);
+
+
+		m_SquareRotation += (float)ts * glm::vec3(1, 2, 3);
+
+
 		m_Camera->SetPosition(m_CameraPosition);
 		m_Camera->SetRotation(m_CameraRotation);
 
@@ -156,6 +175,7 @@ public:
 		// m_Texture->Bind();
 		m_Shader->SetFloat3("u_LightPos", m_LightPos);
 		GameEngine::Renderer::Submit(m_Shader, m_VertexArray, m_Transform);
+		GameEngine::Renderer::Submit(m_Shader, m_PlaneVertexArray, m_PlaneTransform);
 
 
 		// END RENDERING SCENE
@@ -177,7 +197,9 @@ public:
 	{
 		ImGui::Begin("Camera Controls");
 		ImGui::SliderFloat3("Camera Position", glm::value_ptr(m_CameraPosition), -5.0f, 5.0f);
-		ImGui::SliderFloat3("Camera Rotation", glm::value_ptr(m_CameraRotation), -3.14f, 3.14f);
+		ImGui::SliderFloat3("Camera Rotation", glm::value_ptr(m_CameraRotation), -3.14f,3.14f);
+		//ImGui::SliderFloat("Camera Sensitivity", &m_CameraSensitivity, 0.1f, 10.0f); 
+		//ImGui::SliderFloat("Camera Smoothing", &m_CameraSmoothing, 0.1f, 10.0f);
 
 		if (ImGui::Button("Reset"))
 		{
@@ -207,7 +229,7 @@ public:
 
 		if (ImGui::Button("Reset"))
 		{
-			m_LightPos = glm::vec3(0);
+			m_LightPos = glm::vec3(0.0f, 3.0f, 0.0f);
 		}
 
 		ImGui::End();
@@ -216,6 +238,8 @@ public:
 private:
 	std::shared_ptr<GameEngine::VertexArray> m_VertexArray;
 	std::shared_ptr<GameEngine::Shader> m_Shader;
+
+	std::shared_ptr<GameEngine::VertexArray> m_PlaneVertexArray;
 
 	std::shared_ptr<GameEngine::Texture2D> m_Texture;
 
@@ -228,7 +252,14 @@ private:
 	glm::vec3 m_SquareRotation;
 	glm::vec3 m_SquareScale;
 
+	std::shared_ptr<GameEngine::Transform> m_PlaneTransform;
+
 	glm::vec3 m_LightPos;
+
+	float m_CameraSensitivity = 0.5f;
+	float m_CameraSmoothing = 1;
+	glm::vec2 m_CameraSmoothVector;
+	glm::vec2 m_MouseLook;
 };
 
 
